56ae46c535a45e464547569ce5113be4368e6095
kamlesh <ks6849663@gmail.com>
2021-09-30 22:51:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
+const request = require("request");
+const cheerio = require("cheerio");
+
+let scoreCardObj = require("./scoreCard");
+request(url, cb);
+
+function cb(err, res, html) {
+  if (!err) {
+    let searchTool = cheerio.load(html);
+    let link = searchTool(".widget-items.cta-link a");
+    let fullLink = "https://www.espncricinfo.com" + link.attr("href");
+    // console.log(fullLink);
+    request(fullLink, allMatchPageCb);
+  }
+}
+
+function allMatchPageCb(err, res, html) {
+  if (!err) {
+    let $ = cheerio.load(html);
+    let allScoreCardLink = $('a[data-hover="Scorecard"]');
+    // console.log(allScoreCardLink.length);
+    let count = 0;
+    for(let i=0 ; i<allScoreCardLink.length ; i++){
+        let link = $(allScoreCardLink[i]).attr("href");
+        let fullAllMatchLink ="https://www.espncricinfo.com"+link;
+        // console.log("https://www.espncricinfo.com"+link);
+        scoreCardObj.processSingleMatch(fullAllMatchLink)
+    }
+    console.log("```````````````````````````````````");
+  }
+}
---
+++ scoreCard.js
@@ -0,0 +1,47 @@
+// const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+function processSingleMatch(url){
+    request(url,cb);
+
+}
+
+function cb(err,res,html){
+    if(!err){
+        let $ = cheerio.load(html);
+        let scorecardArr = $(".match-scorecard-page .Collapsible");
+        // console.log(scorecardArr.length);
+        let scorecard = ""
+        for(let i=0 ; i<scorecardArr.length ; i++){
+            // console.log(scorecardArr[i]);
+            // console.log("````````````````````````````");
+            // fs.writeFileSync(`innings${i+1}.html`,$(scorecardArr[i]).html());
+            let teamNameElement = $(scorecardArr[i]).find("h5");
+            let teamName = teamNameElement.text();
+            teamName = teamName.split("INNINGS")[0];
+            teamName = teamName.trim();
+            // console.log(teamName);
+            let batsmanTableBodyAllRow = $(scorecardArr[i]).find(".table.batsman tbody tr");
+            // console.log(batsmanTableBodyAllRow.length);
+            for(let i=0 ; i<batsmanTableBodyAllRow.length ; i++){
+                let numberOfTds = $(batsmanTableBodyAllRow[i]).find("td");
+                // console.log(numberOfTds.length);
+                if(numberOfTds.length == 8){
+                    let playerName = $(numberOfTds[0]).text()
+                    console.log(playerName); 
+                }
+            }
+
+            console.log("```````````````````````````````````");
+
+        }
+        
+        
+    }
+}
+
+module.exports = {
+    processSingleMatch
+}
