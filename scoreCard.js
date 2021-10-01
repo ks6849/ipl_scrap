be6db79c9750f97d9a10367aa31d31fd24a221f7
akashsharmab23 <91763110+akashsharmab23@users.noreply.github.com>
2021-10-02 04:11:41 +0530

    scorecard.js updated

--- scoreCard.js
+++ scoreCard.js
@@ -88,6 +88,22 @@ function processSingleMatch(url){
                     
                     console.log(a+b);
                 }
+    
+    function processSingleMatch(url){
+    request(url,cb);
+    console.log(''''''''''''''');
+                console.log(''''''&&&&''''''''');
+                console.log(''''''''''''''');
+                console.log(''''''''''''''');
+                console.log(''''''''''''''');
+                console.log(''''''''''''''');
+                console.log(''''''''''''''');
+                let a = 0;
+                let b = 5;
+                for(let i=0 ; i<10 ; i++){
+                    
+                    console.log(a+b);
+                }
                 
 }
 
