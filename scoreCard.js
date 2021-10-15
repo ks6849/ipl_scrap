1221712410cc1cf420c9c595f877935d3712bc22
v1075 <92587041+v1075@users.noreply.github.com>
2021-10-15 19:09:06 +0530

    updated scorecard functions

--- scoreCard.js
+++ scoreCard.js
@@ -27,6 +27,9 @@ function processSingleMatch(url){
     for(let i=0 ; i<10 ; i++){
                     console.log(a+b);
                 } 
+    for(let i=0 ; i<10 ; i++){
+                    console.log(a+b);
+                } 
 }
 function processSingleMatch(url){
     request(url,cb);
